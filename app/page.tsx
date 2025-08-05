3b94f1109a31cb8d07e5488ea06e561563fea5d1
DESKTOP-QINRQIA\nicol <nicolepanggg@gmail.com>
2025-08-05 12:12:48 +0800

    Strapi

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+async function getStrapiData(patch:string) {
+  const baseUrl = "http://localhost:1337";
+  try{
+    const response = await fetch(baseUrl + patch);
+    const data = await response.json();
+    return data;
+  } catch(error){
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  const {title, description} = strapiData.data;
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="container mx-auto py-6">
+      <h1 className="text-3xl font-bold underline">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
+    </main>
   );
 }
