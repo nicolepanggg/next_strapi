fbafd120e724984d0c04639420c54ed1dbd9f0d4
DESKTOP-QINRQIA\nicol <nicolepanggg@gmail.com>
2025-08-05 10:09:52 +0800

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container:{
+      center: true,
+      padding:"2rem",
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1800px',
+        '4xl': '2100px',
+      }
+    }
   },
   plugins: [],
 }
