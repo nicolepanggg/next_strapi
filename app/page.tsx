ced0888fda7288da5f877fef6e0d00dd423941bd
DESKTOP-QINRQIA\nicol <nicolepanggg@gmail.com>
2025-08-10 23:50:24 +0800

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,47 @@
+import qs from 'qs';
 
-async function getStrapiData(patch:string) {
+const homePageQuery = qs.stringify({
+      populate: {
+        blocks: {
+          populate: '*',
+        },
+      },
+})
+
+async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337";
-  try{
-    const response = await fetch(baseUrl + patch);
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  console.log(url.href);
+
+  try {
+    const response = await fetch(url.href);
     const data = await response.json();
+    // console.log(data);
+    console.dir(data, { depth: null });
     return data;
-  } catch(error){
+  } catch (error) {
     console.error(error);
   }
 }
 
 export default async function Home() {
-  const strapiData = await getStrapiData("/api/home-page");
-  const {title, description} = strapiData.data;
-  return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-3xl font-bold underline">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
-    </main>
-  );
+  try {
+    const homePageApiData = await getStrapiData("/api/home-page");
+    if (!homePageApiData?.data) {
+      return <div>Error: No data found</div>;
+    }
+    const { title, description } = homePageApiData.data;
+
+    return (
+      <main className="container mx-auto py-6">
+        <h1 className="text-3xl font-bold underline">{title}</h1>
+        <p className="text-xl mt-4">{description}</p>
+      </main>
+    );
+  } catch (error) {
+    return <div>Error: Failed to load data from Strapi</div>;
+  }
 }
